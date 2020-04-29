a8d7a6b0b6bef25a596483913366ec401b17858d
Nayan <Nayan@DESKTOP-2379B9P>
2020-04-29 21:46:27 +0530

    Controllers for services

---
+++ nach/src/main/java/com/api/nach/resources/AccountHolderController.java
@@ -0,0 +1,40 @@
+package com.api.nach.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.api.nach.repos.PrevIinRepository;
+
+@Controller
+public class AccountHolderController {
+	
+	@Autowired
+	private PrevIinRepository prevIinRepo;
+	
+	@GetMapping(value="/home2")
+	public String aadhaarDtls() {
+		
+		return "acctHolder";
+	}
+	
+	@PostMapping("/acctHolderDtls")
+	  public void getData(@RequestBody String data) {
+		String acctHolderData = data.replace("=", "").replace("%2C", ",").replace("%2F", "/");
+		System.out.println("Holder data "+acctHolderData);
+	}
+	
+	 @ModelAttribute("previinlist")
+	   public List<String> getWebFrameworkList() {
+	      List<String> prevIinList = new ArrayList<String>();
+	      prevIinList = prevIinRepo.findAllIin();
+	      return prevIinList;
+	   }
+
+}
