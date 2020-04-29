a926743ba46ba075ce5ab266812dcbd50dcebba8
Nayan <Nayan@DESKTOP-2379B9P>
2020-04-29 21:52:32 +0530

    Some changes made for deployment to dedicated Tomcat Container

--- nach/src/main/java/com/api/nach/NachApiApplication.java
+++ nach/src/main/java/com/api/nach/NachApiApplication.java
@@ -2,12 +2,20 @@ package com.api.nach;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NachApiApplication {
+public class NachApiApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(NachApiApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure (SpringApplicationBuilder builder) {
+		
+		return builder.sources(NachApiApplication.class);
 	}
 
 }
