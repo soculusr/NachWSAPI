064e8e88a9e79d1dcacff68421d7373811d17a9f
Nayan <Nayan@DESKTOP-2379B9P>
2020-05-01 13:49:27 +0530

    Signature

--- nach/src/main/java/com/api/nach/services/XmlSigning.java
+++ nach/src/main/java/com/api/nach/services/XmlSigning.java
@@ -3,16 +3,25 @@ package com.api.nach.services;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.security.InvalidAlgorithmParameterException;
+import java.security.Key;
 import java.security.KeyException;
+import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyStore;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.cert.CertStore;
 import java.security.cert.Certificate;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 import java.util.Collections;
 
 import javax.xml.crypto.MarshalException;
@@ -26,6 +35,7 @@ import javax.xml.crypto.dsig.XMLSignature;
 import javax.xml.crypto.dsig.XMLSignatureException;
 import javax.xml.crypto.dsig.XMLSignatureFactory;
 import javax.xml.crypto.dsig.dom.DOMSignContext;
+import javax.xml.crypto.dsig.dom.DOMValidateContext;
 import javax.xml.crypto.dsig.keyinfo.KeyInfo;
 import javax.xml.crypto.dsig.keyinfo.KeyInfoFactory;
 import javax.xml.crypto.dsig.keyinfo.KeyValue;
@@ -41,19 +51,16 @@ import javax.xml.transform.stream.StreamResult;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
 @Service
 public class XmlSigning {
 	
-	public String getSignedData(String xmlData) {
+	public static String getSignedData(String xmlData) {
 		
 		
 		String FilePath = "keys" + File.separator + "nayan.p12";
-		//String FilePath =File.separator +"nayan.p12";
-		//String FilePath = "com" + File.separator +"api"+File.separator+"nach"+File.separator+"keys"+File.separator+ "nayan.p12";
-		String currentWorkingDir = System.getProperty("user.dir");
-        System.out.println("Current Working Directory"+currentWorkingDir);
 		String signedData  = null;
 		KeyInfo keyInfo = null;
         KeyValue keyValue = null;
@@ -63,9 +70,8 @@ public class XmlSigning {
 			
 			DocumentBuilderFactory docBuildFac = DocumentBuilderFactory.newInstance();
 			DocumentBuilder docBuild = docBuildFac.newDocumentBuilder();
-			//Document xmlDoc = docBuild.parse(new ByteArrayInputStream(xmlData.getBytes("UTF-8")));
+	
 			Document xmlDoc = docBuild.parse(new InputSource(new StringReader(xmlData)));
-			//xmlDoc.setXmlStandalone(true);
 			
 			//Load the keystore containing the keys
 			
@@ -75,15 +81,21 @@ public class XmlSigning {
 			
 			keystore.load(new FileInputStream(FilePath),password);
 			
-			//KeyPair key  = getKeyPair(keystore,"mykey",password);
+			String aliasName = "mykey";
+			Certificate cert = keystore.getCertificate(aliasName);
 			
-			String mykey = "mykey";
-			Certificate cert = keystore.getCertificate(mykey);
-
+			Key key = keystore.getKey(aliasName, password);
+			
 			PublicKey publicKey = cert.getPublicKey();
-		
-			PrivateKey privatekey = (PrivateKey)keystore.getKey(mykey , password);
 			
+			KeyPair keypair = new KeyPair(publicKey, (PrivateKey) key);
+			//PrivateKey privatekey= keypair.getPrivate();
+			
+		
+			PrivateKey privatekey = (PrivateKey)keystore.getKey(aliasName , password);
+			
+			
+			System.out.println("Private key "+privatekey);
 			DOMSignContext domSignCtx = new DOMSignContext(privatekey, xmlDoc.getDocumentElement());
 			
 			XMLSignatureFactory xmlSigFactory = XMLSignatureFactory.getInstance("DOM");
@@ -110,7 +122,7 @@ public class XmlSigning {
 	        KeyInfoFactory keyInfoFact = xmlSigFactory.getKeyInfoFactory();
 
 	        try {
-	            keyValue = keyInfoFact.newKeyValue(publicKey);
+	            keyValue = keyInfoFact.newKeyValue(keypair.getPublic());
 	        } catch (KeyException ex) {
 	            ex.printStackTrace();
 	        }
@@ -147,8 +159,125 @@ public class XmlSigning {
 		return signedData;
 	}
 	
+	private static byte[] getKeyData(String filePath) {
+        File file = new File(filePath);
+        byte[] buffer = new byte[(int) file.length()];
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(file);
+            fis.read(buffer);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return buffer;
+    }
 	
-	/*public static void main(String[] args) {
+	public PrivateKey getStoredPrivateKey(String filePath) {
+		
+		PrivateKey privateKey = null;
+        byte[] keydata = getKeyData(filePath);
+        PKCS8EncodedKeySpec encodedPrivateKey = new PKCS8EncodedKeySpec(keydata);
+        KeyFactory keyFactory = null;
+        try {
+            keyFactory = KeyFactory.getInstance("RSA");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        try {
+            privateKey = keyFactory.generatePrivate(encodedPrivateKey);
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+        return privateKey;
+    }
+	
+	public static PublicKey getStoredPublicKey(String filePath) {
+		
+		/*String publicKeyData = "-----BEGIN PUBLIC KEY-----\r\n" + 
+				"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAp/6IUw9H2VdWPpM6pP9k\r\n" + 
+				"pCylsECejWlieo+I00kcAHVNN+oyUNJ2vcuen3OSsIPh1byrsaU6ku0AxXWhr3qf\r\n" + 
+				"0uAAfCTeVRpZjaoDPLVCX/F+gu+4xXeErfttg0syOX6DdeNWucW1ja231pQGGZis\r\n" + 
+				"iTfFdJ0aWn4Tc15DaxIkqhdWYBiGrNT9cCuzXNo3g2UIvhKJh5/Dr1GVV4SXlBC3\r\n" + 
+				"L/O7lc1iGPq/utq0uIZRLEC+e9M/EzH22psV7E99qMS6DPhg5rn6m0iP3K34iM5M\r\n" + 
+				"tRogTGxuluN4TjrYTm9tsSfPd2guoRKF3aEg5yIaS02X05hPVD2DyseBqZK/Exug\r\n" + 
+				"zwIDAQAB\r\n" + 
+				"-----END PUBLIC KEY-----";
+		
+		String pubKeyPEM = publicKeyData.replace("-----BEGIN PUBLIC KEY-----\r\n", "").replace("-----END PUBLIC KEY-----", "");
+		
+		byte[] encodedPublicKeyData = Base64.getMimeDecoder().decode(pubKeyPEM);*/
+        PublicKey publicKey = null;
+        byte[] keydata = getKeyData(filePath);
+        KeyFactory keyFactory = null;
+        try {
+            keyFactory = KeyFactory.getInstance("RSA");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        X509EncodedKeySpec encodedPublicKey = new X509EncodedKeySpec(keydata);
+        try {
+            publicKey = keyFactory.generatePublic(encodedPublicKey);
+        } catch (NullPointerException npe) {
+            npe.printStackTrace();
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+        return publicKey;
+    }
+	
+	public static boolean isXmlDigitalSignatureValid(String signedXmlData) throws Exception {
+		String FilePath = "keys" + File.separator + "nayan.p12";
+		//String pubicKeyFilePath = "keys" + File.separator + "publickey.pem";
+		
+		KeyStore keystore = KeyStore.getInstance("PKCS12");
+		
+		char[] password = "nayan123".toCharArray();
+		
+		keystore.load(new FileInputStream(FilePath),password);
+		
+		String aliasName = "mykey";
+		Certificate cert = keystore.getCertificate(aliasName);
+		PublicKey publicKey = cert.getPublicKey();
+		System.out.println("Public 1 "+publicKey);
+		Key key = keystore.getKey(aliasName, password);
+		KeyPair keypair = new KeyPair(publicKey, (PrivateKey) key);
+		System.out.println("Public 2 "+keypair.getPublic());
+		//PublicKey publicKey = getStoredPublicKey(pubicKeyFilePath);
+		
+		
+			boolean validFlag = false;
+			
+			//load the xml doc to VERIFY sign
+			
+			DocumentBuilderFactory docBuildFac = DocumentBuilderFactory.newInstance();
+			docBuildFac.setNamespaceAware(true);
+			DocumentBuilder docBuild = docBuildFac.newDocumentBuilder();
+			Document xmlDoc = docBuild.parse(new InputSource(new StringReader(signedXmlData)));
+			NodeList nl = xmlDoc.getElementsByTagNameNS(XMLSignature.XMLNS, "Signature");
+			System.out.println("Node length is "+nl.getLength());
+			if (nl.getLength() == 0) {
+			throw new Exception("No XML Digital Signature Found, document is discarded");
+				
+			}
+			DOMValidateContext valContext = new DOMValidateContext(keypair.getPublic(), nl.item(0));
+			XMLSignatureFactory fac = XMLSignatureFactory.getInstance("DOM");
+			XMLSignature signature = fac.unmarshalXMLSignature(valContext);
+			validFlag = signature.validate(valContext);
+			return validFlag;
+			}
+	
+	
+	public static void main(String[] args) throws Exception {
 		
 		String xml = "<ach:GetPanDtlsRqst xmlns:ach=\"http://npci.org/ach/schema/\" >\r\n" + 
 				"			<Head ver=\"1.0\" ts=\"2018-02-12T10:11:00\" />	\r\n" + 
@@ -160,8 +289,15 @@ public class XmlSigning {
 				"			</ReqData>	\r\n" + 
 				"			<NpciRefId value=\"\" />\r\n" + 
 				"		</ach:GetPanDtlsRqst>";
-		System.out.println(getSignedData(xml));
 		
-	}*/
+		
+		String signedXmlData = getSignedData(xml);
+		System.out.println(getSignedData(xml));
+		Boolean flag = isXmlDigitalSignatureValid(signedXmlData);
+		
+		System.out.println("Verify status is" + flag);
+		
+		
+	}
 
 }
